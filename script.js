eafd2b007a36b4afa2ef9c633749c178d25a0fad
creativeact <128939729+creativeact@users.noreply.github.com>
2024-08-09 11:36:23 -0600

    Initial Add Book Modal Prototype
    Create .add-book-modal that appears after clicking on the
    .add-book-button

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// Declare myLibrary Array
 const myLibary = [];
-const addBookButton = document.querySelector('.add-book-button');
 
+// Declare addBook and submitNewBook buttons
+const addBookButton = document.querySelector('.add-book-button');
+const addBookModal = document.querySelector('.add-book-modal');
+const submitNewBook = document.querySelector('.submit-new-book');
+
+// Book Constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -11,12 +17,19 @@ function Book(title, author, pages, read) {
     }
 };
 
+// Add book to Library
 function addBookToLibrary(Book) {
     myLibrary.push(Book);
 };
 
+// Display book
 function displayBook() {
     for (const property in Book) {
         return this.property
     }
+};
+
+// Trigger the Add Book Modal
+addBookButton.onclick = function () {
+    addBookModal.style.display = "block";
 };
