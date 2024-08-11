60da3299fe0cdd98948637d16056236052663f2d
creativeact <128939729+creativeact@users.noreply.github.com>
2024-08-11 16:46:39 -0600

    Implement new book modal functionality
    Adjusted new-book-form in index.html to better handle form submission
    
    Added event listeners in script.js to get form values and
    addBookToLibrary upon form submission
    
    Created closeModal function to handle modal behavior after form
    submission

--- script.js
+++ script.js
@@ -1,35 +1,65 @@
 // Declare myLibrary Array
-const myLibary = [];
+const myLibrary = [];
 
-// Declare addBook and submitNewBook buttons
+// Declare DOM elements
 const addBookButton = document.querySelector('.add-book-button');
-const addBookModal = document.querySelector('.add-book-modal');
-const submitNewBook = document.querySelector('.submit-new-book');
+const addBookModal = document.querySelector('.modal-container');
+const submitBookButton = document.querySelector('.submit-book-button');
+const closeModalButton = document.querySelector('.close-modal-button');
+const newBookForm = document.querySelector('.new-book-form');
 
 // Book Constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    //this.read = read;
     this.info = function () {
-        return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`
+        return `${this.title}, ${this.author}, ${this.pages}`;
     }
 };
 
-// Add book to Library
-function addBookToLibrary(Book) {
-    myLibrary.push(Book);
+// Declare addBookToLibrary function
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 };
 
-// Display book
+// Handle new book form submission
+newBookForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+
+    // Create newBook object with form values
+    const newBook = new Book(title, author, pages);
+
+    addBookToLibrary(newBook);
+
+    // Reset form values and close modal
+    newBookForm.reset();
+    closeModal();
+});
+
+// Display book prototype
 function displayBook() {
     for (const property in Book) {
         return this.property
     }
 };
 
-// Trigger the Add Book Modal
+// Open Add Book Modal
 addBookButton.onclick = function () {
-    addBookModal.style.display = "block";
+    addBookModal.style.display = "grid";
+};
+
+// Declare Close Modal function;
+function closeModal() {
+    addBookModal.style.display = "none";
+};
+
+closeModalButton.onclick = function () {
+    closeModal();
 };
